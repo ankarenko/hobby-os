2b57a0f14fee999e27e96d346ca4fb7219133374
ankarenko <ankarenkosergey@gmail.com>
2023-11-24 00:24:27 -0300

    save

--- tools/configure_libc.sh
+++ tools/configure_libc.sh
@@ -1,3 +1,4 @@
+mkdir /src/build-newlib && \
 cd /src/newlib-2.5.0/newlib/libc/sys && autoconf && \
 cd myos && autoreconf && cd /src/build-newlib && \
 ../newlib-2.5.0/configure --prefix=/usr --target=i686-myos
